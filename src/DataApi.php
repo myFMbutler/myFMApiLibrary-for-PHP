5533bdd798a9a05296ed222a111d9e9b860b506e
lguilbert <39915415+lguilbert@users.noreply.github.com>
2019-05-06 15:31:26 +0200

    fix: filename can be empty
    Let options to not reset filename when uploading in container.

--- src/DataApi.php
+++ src/DataApi.php
@@ -269,14 +269,18 @@ final class DataApi implements DataApiInterface
      * @param $containerFieldName
      * @param $containerFieldRepetition
      * @param $filepath
-     * @param $filename
+     * @param null $filename
      *
      * @return true
      *
      * @throws Exception
      */
-    public function uploadToContainer($layout, $recordId, $containerFieldName, $containerFieldRepetition, $filepath, $filename)
+    public function uploadToContainer($layout, $recordId, $containerFieldName, $containerFieldRepetition, $filepath, $filename = null)
     {
+        if (empty($filename)) {
+            $filename = pathinfo($filepath, PATHINFO_FILENAME).'.'.pathinfo($filepath, PATHINFO_EXTENSION);
+        }
+        
         $this->ClientRequest->request(
             'POST',
             "/v1/databases/$this->apiDatabase/layouts/$layout/records/$recordId/containers/$containerFieldName/$containerFieldRepetition",
